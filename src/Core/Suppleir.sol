5fc15efcfc986b1e9b637b47c09e9d49be120d5a
Addis Belete <addis.bel@gmail.com>
2022-10-31 22:25:15 +0300

    Add Suppleir contract

---
+++ src/Core/Suppleir.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.8.7;
+
+/**
+ * @notice Is a contract used to manage suppliers profile
+ * @title Suppleirs contract
+ * @author Addis Belete
+ */
+contract Suppleirs {
+
+}
